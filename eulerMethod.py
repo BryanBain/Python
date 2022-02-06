ff037ce441d759ad8ab75d9346c941aea4cddfc6
Bryan Bain <BryanBain@users.noreply.github.com>
2022-02-06 13:38:39 -0500

    Updated eulerMethod.py

--- eulerMethod.py
+++ eulerMethod.py
@@ -8,22 +8,19 @@ File Name: eulerMethod.py
 import numpy as np
 import math
 
-# Initial conditions
-x = 1
-y = -1
+def eulerMethod(x_0, y_0, h, x_n, func):
+	"""
+	:param x_0: initial x value
+	:param y_0: initial y value
+	:param h: step size
+	:param x_n: last value of x
+	:param func: lambda function that y' equals
+	"""
+	num_iters = int((x_n-x_0)/h)+1
+	print(f'x \t\t y \t\t\t f(x,y)')
+	for i in range(num_iters):
+  		print(f'{x_0:0.2f} \t {y_0:0.6f} \t {func(x_0,y_0):0.6f}')
+  		y_0 += h*func(x_0,y_0)
+  		x_0 += h
 
-dx = 0.5  # Step size
-
-x_n = 5  # Last value of x
-
-# The function itself
-f = lambda x,y: y**2/np.sqrt(x)
-
-# number of iterations
-num_iters = int((x_n-x)/dx)+1
-
-print(f'x \t\t y \t\t\t f(x,y)')
-for i in range(num_iters):
-  print(f'{x:0.2f} \t {y:0.6f} \t {f(x,y):0.6f}')
-  y += dx*f(x,y)
-  x += dx
+eulerMethod(0, 1, 0.1, 1, lambda x,y: x-y)
